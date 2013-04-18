5cc8e3fc8ec47cea25a3afd3074de86cae6d163a
jcredding <TempestTTU@gmail.com>
2013-04-18 17:09:27 -0500

    Add ability to configure most of Sinatra's settings
    This add's the ability to set most of Sinatra's settings through
    Deas. This allows some customization of the Sinatra app but only
    through Deas API. The main intent is to allow customization of
    things like serving static files, supporting sessions and
    changing paths to the public or views folder.

--- lib/deas/sinatra_app.rb
+++ lib/deas/sinatra_app.rb
@@ -7,7 +7,22 @@ module Deas
     def self.new(server_config)
       server_config.init_proc.call
 
-      Class.new(Sinatra::Base)
+      Class.new(Sinatra::Base).tap do |app|
+
+        app.set :environment, server_config.env
+        app.set :root,        server_config.root
+
+        app.set :app_file,      server_config.app_file
+        app.set :public_folder, server_config.public_folder
+        app.set :views,         server_config.views_folder
+
+        app.set :dump_errors,     server_config.dump_errors
+        app.set :logging,         false
+        app.set :method_override, server_config.method_override
+        app.set :sessions,        server_config.sessions
+        app.set :static,          server_config.static_files
+
+      end
     end
 
   end
--- test/unit/server_tests.rb
+++ test/unit/server_tests.rb
@@ -5,6 +5,14 @@ class Deas::Server
 
   class BaseTests < Assert::Context
     desc "Deas::Server"
+    setup do
+      @old_configuration = Deas::Server.configuration.dup
+      new_configuration = Deas::Server::Configuration.new
+      Deas::Server.instance_variable_set("@configuration", new_configuration)
+    end
+    teardown do
+      Deas::Server.instance_variable_set("@configuration", @old_configuration)
+    end
     subject{ Deas::Server }
 
     should have_instance_methods :configuration, :init
@@ -14,6 +22,9 @@ class Deas::Server
     end
 
     should "allow setting it's configuration options" do
+      subject.env 'staging'
+      assert_equal 'staging', subject.configuration.env
+
       init_proc = proc{ }
       subject.init(&init_proc)
       assert_equal init_proc, subject.configuration.init_proc
@@ -28,7 +39,41 @@ class Deas::Server
     end
     subject{ @configuration }
 
-    should have_instance_methods :init_proc
+    should have_instance_methods :env, :root, :app_file, :public_folder,
+      :views_folder, :dump_errors, :method_override, :sessions, :static_files,
+      :init_proc
+
+    should "default the env to 'development'" do
+      assert_equal 'development', subject.env
+    end
+
+    should "default the root to the routes file's folder" do
+      expected_root = File.expand_path('..', Deas.config.routes_file)
+      assert_equal expected_root, subject.root.to_s
+    end
+
+    should "default the app file to the routes file" do
+      assert_equal Deas.config.routes_file.to_s, subject.app_file.to_s
+    end
+
+    should "default the public folder based on the root" do
+      expected_root = File.expand_path('..', Deas.config.routes_file)
+      expected_public_folder = File.join(expected_root, 'public')
+      assert_equal expected_public_folder, subject.public_folder.to_s
+    end
+
+    should "default the views folder based on the root" do
+      expected_root = File.expand_path('..', Deas.config.routes_file)
+      expected_views_folder = File.join(expected_root, 'views')
+      assert_equal expected_views_folder, subject.views_folder.to_s
+    end
+
+    should "default the Sinatra flags" do
+      assert_equal false, subject.dump_errors
+      assert_equal true,  subject.method_override
+      assert_equal true,  subject.sessions
+      assert_equal true,  subject.static_files
+    end
 
   end
 
