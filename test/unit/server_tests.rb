82ab5f5d48b640e512fa07ced1193158bcefa759
jcredding <TempestTTU@gmail.com>
2013-04-18 14:49:50 -0500

    Add `init` proc to `Deas::Server`
    This adds the ability configure an `init` proc on `Deas::Server`.
    This proc is then called before the sinatra app is built. This
    allows users to define initialization logic for their server.

--- test/unit/server_tests.rb
+++ test/unit/server_tests.rb
@@ -7,9 +7,28 @@ class Deas::Server
     desc "Deas::Server"
     subject{ Deas::Server }
 
+    should have_instance_methods :configuration, :init
+
     should "be a singleton" do
       assert_includes Singleton, subject.included_modules
     end
+
+    should "allow setting it's configuration options" do
+      init_proc = proc{ }
+      subject.init(&init_proc)
+      assert_equal init_proc, subject.configuration.init_proc
+    end
+
+  end
+
+  class ConfigurationTests < BaseTests
+    desc "Configuration"
+    setup do
+      @configuration = Deas::Server.configuration
+    end
+    subject{ @configuration }
+
+    should have_instance_methods :init_proc
 
   end
 
