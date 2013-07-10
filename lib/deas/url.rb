ac1b955141014230107385cb27c40e5f0099635d
Kelly Redding <kelly@kellyredding.com>
2013-07-09 20:26:56 -0500

    have Urls generate paths given named/ordered params
    This adds behavior so that a Url can generate a path for given
    parameter data.  Params can be name (given as a Hash) or ordered
    and will be applied to the source path appropriately.
    
    This also adds a convenience `url` method on the server for looking
    up a named url and generating a path given some params data.

--- lib/deas/url.rb
+++ lib/deas/url.rb
@@ -7,5 +7,24 @@ module Deas
       @name, @path = name, path
     end
 
+    def path_for(*args)
+      named, ordered = [
+        args.last.kind_of?(::Hash) ? args.pop : {},
+        args
+      ]
+      apply_ordered_params(apply_named_params(@path, named), ordered)
+    end
+
+    private
+
+    def apply_named_params(path, params)
+      splat = params[:splat] || params['splat'] || '*'
+      params.inject(path){ |s, (k, v)| s.gsub(":#{k}", v) }.gsub("*", splat)
+    end
+
+    def apply_ordered_params(path, params)
+      params.inject(path){ |s, p| s.sub(/\*+|\:\w+/i, p) }
+    end
+
   end
 end
--- test/unit/url_tests.rb
+++ test/unit/url_tests.rb
@@ -13,10 +13,63 @@ class Deas::Url
     subject{ @url }
 
     should have_readers :name, :path
+    should have_imeth :path_for
 
     should "know its name and path info" do
       assert_equal :get_info, subject.name
       assert_equal '/info', subject.path
+    end
+
+  end
+
+  class PathForTests < BaseTests
+    desc "when generating paths"
+    setup do
+      @url = Deas::Url.new(:some_thing, '/:some/:thing/*')
+    end
+
+    should "generate given named params only" do
+      exp_path = "/a/goose/*"
+      assert_equal exp_path, subject.path_for({
+        'some' => 'a',
+        :thing => 'goose'
+      })
+
+      exp_path = "/a/goose/cooked/well"
+      assert_equal exp_path, subject.path_for({
+        'some'  => 'a',
+        :thing  => 'goose',
+        'splat' => 'cooked/well'
+      })
+    end
+
+    should "generate given ordered params only" do
+      exp_path = "/a/:thing/*"
+      assert_equal exp_path, subject.path_for('a')
+
+      exp_path = "/a/goose/*"
+      assert_equal exp_path, subject.path_for('a', 'goose')
+
+      exp_path = "/a/goose/cooked/well"
+      assert_equal exp_path, subject.path_for('a', 'goose', 'cooked/well')
+    end
+
+    should "generate given mixed ordered and named params" do
+      exp_path = "/:some/:thing/*"
+      assert_equal exp_path, subject.path_for
+
+      exp_path = "/a/goose/*"
+      assert_equal exp_path, subject.path_for('a', 'thing' => 'goose')
+
+      exp_path = "/goose/a/well"
+      assert_equal exp_path, subject.path_for('a', 'well', 'some' => 'goose')
+
+      exp_path = "/a/goose/cooked/well"
+      assert_equal exp_path, subject.path_for('these', 'are', 'ignored', {
+        'some'  => 'a',
+        :thing  => 'goose',
+        'splat' => 'cooked/well'
+      })
     end
 
   end
